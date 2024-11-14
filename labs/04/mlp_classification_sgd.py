22a4f31780327cf3955fe262d457bb1f45f8d9c4
Vit Babka <vitek.babka@seznam.cz>
2024-11-14 13:58:31 +0100

    Added 04

--- labs/04/mlp_classification_sgd.py
+++ labs/04/mlp_classification_sgd.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# f5419161-0138-4909-8252-ba9794a63e53
+# 964bdfc8-60b0-4398-b837-7c2520532d17
+# 4b50a6fb-a4a6-4b30-9879-0b671f941a72
 import argparse
 
 import numpy as np
@@ -37,6 +40,18 @@ def main(args: argparse.Namespace) -> tuple[tuple[np.ndarray, ...], list[float]]
                generator.uniform(size=[args.hidden_layer, args.classes], low=-0.1, high=0.1)]
     biases = [np.zeros(args.hidden_layer), np.zeros(args.classes)]
 
+    def ReLU(X):
+        return np.maximum(0, X)
+    
+    def softmax(X):
+        X -= np.max(X, axis = 1, keepdims=True)
+
+        sum = np.sum(np.exp(X), axis = 1, keepdims=True)
+    
+        return (np.exp(X)) / sum
+
+
+
     def forward(inputs):
         # TODO: Implement forward propagation, returning *both* the value of the hidden
         # layer and the value of the output layer.
@@ -52,11 +67,18 @@ def main(args: argparse.Namespace) -> tuple[tuple[np.ndarray, ...], list[float]]
         # in softmax can easily overflow. To avoid it, you should use the fact that
         # $softmax(z) = softmax(z + any_constant)$ and compute $softmax(z) = softmax(z - maximum_of_z)$.
         # That way we only exponentiate values which are non-positive, and overflow does not occur.
-        raise NotImplementedError()
+
+        hidden_layer = ReLU(inputs @ weights[0] + biases[0])
+        output_layer = softmax(hidden_layer @ weights[1] + biases[1])
+
+        return hidden_layer, output_layer
+    
 
     for epoch in range(args.epochs):
         permutation = generator.permutation(train_data.shape[0])
 
+        permutation_X = train_data[permutation]
+        permutation_Y = train_target[permutation]
         # TODO: Process the data in the order of `permutation`. For every
         # `args.batch_size` of them, average their gradient, and update the weights.
         # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
@@ -74,14 +96,62 @@ def main(args: argparse.Namespace) -> tuple[tuple[np.ndarray, ...], list[float]]
         # - compute the derivative with respect to the hidden layer input,
         # - compute the derivative with respect to `weights[0]` and `biases[0]`.
 
+        for start in range(0, len(train_data), args.batch_size):
+            X = permutation_X[start : start + args.batch_size]
+            Y = permutation_Y[start : start + args.batch_size]
+
+            hidden, predictions = forward(X)
+
+            W0, b0, W1, b1 = compute_gradients(X, hidden, predictions, Y, weights)
+
+            weights[0] -= args.learning_rate * W0
+            weights[1] -= args.learning_rate * W1
+            biases[0] -= args.learning_rate * b0
+            biases[1] -= args.learning_rate * b1
+
+
         # TODO: After the SGD epoch, measure the accuracy for both the
         # train test and the test set.
-        train_accuracy, test_accuracy = ...
+        train_pred = forward(train_data)[1]
+        test_pred = forward(test_data)[1]
+        train_accuracy = compute_accuracy(train_pred, train_target)
+        test_accuracy = compute_accuracy(test_pred, test_target)
 
         print("After epoch {}: train acc {:.1f}%, test acc {:.1f}%".format(
             epoch + 1, 100 * train_accuracy, 100 * test_accuracy))
 
     return tuple(weights + biases), [100 * train_accuracy, 100 * test_accuracy]
+
+def compute_accuracy(predictions, y_true):
+    sum =  0
+    n_samples = predictions.shape[0]
+    for i in range(n_samples):
+        if np.argmax(predictions[i]) == y_true[i]:
+            sum += 1
+    return sum / n_samples
+
+
+def compute_gradients(X, hidden, pred, Y, weights):
+
+    n_samples = X.shape[0]
+    one_hot_Y = np.zeros_like(pred)
+    one_hot_Y[np.arange(n_samples), Y] = 1
+
+    error_out = pred - one_hot_Y
+
+    W1_grad = (hidden.T @ error_out) / n_samples
+    b1_grad = np.sum(error_out, axis=0) / n_samples
+
+    error_hidden = error_out @ weights[1].T     #Backpropagation
+    error_hidden[hidden <= 0] = 0   #ReLU
+
+    W0_grad = (X.T @ error_hidden) / n_samples
+    b0_grad = np.sum(error_hidden, axis=0) / n_samples
+
+    return W0_grad, b0_grad, W1_grad, b1_grad
+
+
+
 
 
 if __name__ == "__main__":
--- labs/04/softmax_classification_sgd.py
+++ labs/04/softmax_classification_sgd.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3``
+# f5419161-0138-4909-8252-ba9794a63e53
+# 964bdfc8-60b0-4398-b837-7c2520532d17
+# 4b50a6fb-a4a6-4b30-9879-0b671f941a72
 import argparse
 
 import numpy as np
@@ -21,7 +24,7 @@ parser.add_argument("--test_size", default=797, type=lambda x: int(x) if x.isdig
 def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]]]:
     # Create a random generator with a given seed.
     generator = np.random.RandomState(args.seed)
-
+    
     # Load the digits dataset.
     data, target = sklearn.datasets.load_digits(n_class=args.classes, return_X_y=True)
 
@@ -41,6 +44,8 @@ def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]
     for epoch in range(args.epochs):
         permutation = generator.permutation(train_data.shape[0])
 
+        
+
         # TODO: Process the data in the order of `permutation`. For every
         # `args.batch_size` of them, average their gradient, and update the weights.
         # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
@@ -50,16 +55,70 @@ def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]
         # $softmax(z) = softmax(z + any_constant)$ and compute $softmax(z) = softmax(z - maximum_of_z)$.
         # That way we only exponentiate non-positive values, and overflow does not occur.
 
+        permutation_X = train_data[permutation]
+        permutation_Y = train_target[permutation]
+
+        for start in range(0, len(train_data), args.batch_size):
+
+            X = permutation_X[start : start + args.batch_size]
+            Y = permutation_Y[start : start + args.batch_size]
+
+            logits = X @ weights
+            predictions = softmax(logits)
+
+            #loss = cross_entropy_loss(predictions, Y)
+
+            gradient = compute_gradient(X, Y, predictions)
+            weights -= args.learning_rate * gradient
+
+
         # TODO: After the SGD epoch, measure the average loss and accuracy for both the
         # train test and the test set. The loss is the average MLE loss (i.e., the
         # negative log-likelihood, or cross-entropy loss, or KL loss) per example.
-        train_accuracy, train_loss, test_accuracy, test_loss = ...
+        train_pred = softmax(train_data @ weights)
+        test_pred = softmax(test_data @ weights)
+
+        train_accuracy = compute_accuracy(train_pred, train_target)
+        train_loss = cross_entropy_loss(train_pred, train_target)
+
+        test_accuracy = compute_accuracy(test_pred, test_target)
+        test_loss = cross_entropy_loss(test_pred, test_target)
+
+
+        #train_accuracy, train_loss, test_accuracy, test_loss = ...
 
         print("After epoch {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(
             epoch + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
 
     return weights, [(train_loss, 100 * train_accuracy), (test_loss, 100 * test_accuracy)]
 
+
+def softmax(logits : np.ndarray):
+    logits -= np.max(logits, axis = 1, keepdims=True)
+
+    sum = np.sum(np.exp(logits), axis = 1, keepdims=True)
+    
+    return (np.exp(logits)) / sum
+
+
+def cross_entropy_loss(predictions, targets):
+
+    true_pred = predictions[range(predictions.shape[0]), targets]
+    true_log_probs = - np.log(true_pred)
+    return np.mean(true_log_probs)
+
+def compute_gradient(X, y_true, predictions):
+    n_samples = predictions.shape[0]
+    predictions[range(n_samples), y_true] -= 1  # predictions - y_true (one_hot)
+    return X.T @ predictions / n_samples
+
+def compute_accuracy(predictions, y_true):
+    sum =  0
+    n_samples = predictions.shape[0]
+    for i in range(n_samples):
+        if np.argmax(predictions[i]) == y_true[i]:
+            sum += 1
+    return sum / n_samples
 
 if __name__ == "__main__":
     main_args = parser.parse_args([] if "__file__" not in globals() else None)
